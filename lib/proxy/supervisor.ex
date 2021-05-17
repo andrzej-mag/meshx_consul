08bf70a849898de834e8f61e40dac0e23a148685
Andrzej Magdziarz <am@am.ventures>
2021-05-17 15:25:59 +0200

    [Proxy] add docs

--- lib/proxy/supervisor.ex
+++ lib/proxy/supervisor.ex
@@ -1,8 +1,25 @@
 defmodule MeshxConsul.Proxy do
   @moduledoc """
-  Sidecar service proxy management.
+  Manages sidecar service proxy binary command.
 
-  todo: Documentation will be provided at a later date.
+  Service mesh data plane is using system of connected proxies managed by control plane application for service communication.
+  Module is managing starting, stopping and restarting proxies binary commands.
+  Proxies should be automatically started running `MeshxConsul.start/4` and stopped with `MeshxConsul.stop/1`.
+
+  Command running proxy binary for given service is passed as service `template` argument to `MeshxConsul.start/4`.
+  Default proxy command will start Consul Connect proxy with error log-level and will use `/bin/sh` as shell:
+  ```elixir
+  [proxy: ["/bin/sh", "-c", "consul connect proxy -log-level err -sidecar-for {{id}}"]]
+  ```
+  Command will be rendered using Mustache system, check `MeshxConsul` documentation for details.
+
+  Example proxy command starting [Envoy Proxy](https://www.envoyproxy.io/):
+  ```elixir
+  [proxy: ["/bin/sh", "-c", "consul connect envoy -sidecar-for {{id}} -- -l error"]]
+  ```
+  **Note:** Consul application version must be compatible with provided Envoy binary, see: [Consul documentation](https://www.consul.io/docs/connect/proxies/envoy#supported-versions).
+
+  Proxy binary command must be able to communicate with Consul: agent instance address and ACL token must be provided. Configuration may be provided as shell environment variable defined by `:cli_env` key in `config.exs` or directly using proxy command. Environment variable is preferred when passing secrets.
   """
 
   use DynamicSupervisor
@@ -12,11 +29,17 @@ defmodule MeshxConsul.Proxy do
   @doc false
   def start_link(init_arg), do: DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
 
-  def start(_service_name, cmd) when is_nil(cmd), do: {:ok, nil}
+  @doc """
+  Starts long-running proxy binary `cmd` for `service_id` service.
+  """
+  @spec start(service_id :: atom() | String.t(), cmd :: [String.t()]) :: DynamicSupervisor.on_start_child() | {:ok, nil}
+  def start(_service_id, cmd) when is_nil(cmd), do: {:ok, nil}
+  # FIXME:
+  # def start(_service_id, cmd) when is_nil(cmd), do: :ignore
 
-  def start(service_name, cmd) do
-    id = id(service_name)
-    spec = Supervisor.child_spec({@worker_mod, [service_name, id, cmd]}, [])
+  def start(service_id, cmd) do
+    id = id(service_id)
+    spec = Supervisor.child_spec({@worker_mod, [service_id, id, cmd]}, [])
 
     case DynamicSupervisor.start_child(__MODULE__, spec) do
       {:ok, pid} -> {:ok, pid}
@@ -25,8 +48,12 @@ defmodule MeshxConsul.Proxy do
     end
   end
 
-  def stop(service_name) do
-    id = id(service_name)
+  @doc """
+  Stops `service_id` service proxy.
+  """
+  @spec stop(service_id :: atom() | String.t()) :: :ok | {:error, :not_found}
+  def stop(service_id) do
+    id = id(service_id)
     @worker_mod.cleanup(id)
 
     if is_pid(Process.whereis(__MODULE__)) do
@@ -37,10 +64,42 @@ defmodule MeshxConsul.Proxy do
     end
   end
 
-  def restart(service_name), do: @worker_mod.restart(id(service_name))
-  def info(service_name), do: @worker_mod.info(id(service_name))
+  @doc """
+  Restarts proxy binary for `service_id` service.
+  ```elixir
+  iex(1)> MeshxConsul.start("service1")
+  {:ok, "service1-h11", {:tcp, {127, 0, 0, 1}, 1024}}
+  iex(2)> MeshxConsul.Proxy.restart("service1-h11")
+  :ok
+  ```
+  """
+  @spec restart(service_id :: atom() | String.t()) :: :ok
+  def restart(service_id), do: @worker_mod.restart(id(service_id))
+
+  @doc """
+  Returns info about `service_id` service proxy worker.
+
+  Function result if successful is map with two keys:
+    * `:cmd` - contains Mustache rendered command that was used to start proxy binary,
+    * `:restarts` - equal to number of proxy restarts due to proxy command failure.
+
+  Exits if `service_id` worker is not running.
+
+  ```elixir
+  iex(1)> MeshxConsul.start("service1")
+  {:ok, "service1-h11", {:tcp, {127, 0, 0, 1}, 1024}}
+  iex(2)> MeshxConsul.Proxy.info("service1-h11")
+  %{
+    cmd: ["/bin/sh", "-c",
+      "consul connect proxy -log-level err -sidecar-for service1-h11"],
+    restarts: 0
+  }
+  ```
+  """
+  @spec info(service_id :: atom() | String.t()) :: %{cmd: String.t(), restarts: non_neg_integer()}
+  def info(service_id), do: @worker_mod.info(id(service_id))
 
   @impl true
   def init(_init_arg), do: DynamicSupervisor.init(strategy: :one_for_one)
-  defp id(service_name), do: Module.concat([__MODULE__, service_name])
+  defp id(service_id), do: Module.concat([__MODULE__, service_id])
 end
